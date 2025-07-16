da5d309cb8a514e9d4fb6768d593416fbee38f1e
Kapil <sharmaakapil01@gmail.com>
2025-07-16 08:31:30 +0530

    feature: added basic rate-limitting to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { HomepageModule } from './homepage/homepage.module';
 import { PrismaService } from './prisma.service';
 import { PrismaModule } from './prisma.module';
@@ -17,12 +17,43 @@ import { BannerModule } from './banner/banner.module';
 import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
 import { ShippingAddressModule } from './shipping-address/shipping-address.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { CustomThrottlerGuard } from './custom-throttler.guard';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true
-  }),HomepageModule, PrismaModule, UserModule, PlpModule, PdpModule, CartModule, DiscountsModule, ReviewModule, ImageModule, CategoryModule, BannerModule, OrderModule, PaymentModule, ShippingAddressModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+    HomepageModule,
+    PrismaModule,
+    UserModule,
+    PlpModule,
+    PdpModule,
+    CartModule,
+    DiscountsModule,
+    ReviewModule,
+    ImageModule,
+    CategoryModule,
+    BannerModule,
+    OrderModule,
+    PaymentModule,
+    ShippingAddressModule,
+  ],
   controllers: [AppController],
-  providers: [AppService,PrismaService],
+  providers: [
+    AppService,
+    PrismaService,
+    { provide: APP_GUARD, useClass: CustomThrottlerGuard },
+  ],
 })
 export class AppModule {}
---
+++ src/custom-throttler.guard.ts
@@ -0,0 +1,10 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
+
+@Injectable()
+export class CustomThrottlerGuard extends ThrottlerGuard {
+  protected async getTracker(req: Record<string,any>): Promise<string> {
+   
+    return Promise.resolve(req.user?.id ?? req.ip);
+  }
+}
